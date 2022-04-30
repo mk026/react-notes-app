bf4284a2fd93a9731ab8cbd9dc6f2ae7b723fdf2
Mikhail <mk0602604@gmail.com>
2022-04-30 22:02:01 +0300

    Render different routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
+import Account from "./pages/Account";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
+import Notes from "./pages/Notes";
+import Todos from "./pages/Todos";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="notes" element={<Notes />} />
+        <Route path="todos" element={<Todos />} />
+        <Route path="account" element={<Account />} />
+        <Route path="auth" element={<Auth />} />
+      </Routes>
+    </>
   );
 }
 
