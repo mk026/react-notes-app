75e152d0825e97c28bfa4773083015c5d34101d2
Mikhail <mk0602604@gmail.com>
2022-04-30 21:52:03 +0300

    Add navlinks to a navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
+import { NavLink } from "react-router-dom";
 
 import classes from "./Navbar.module.css";
 
 const Navbar: FC = () => {
-  return <nav className={classes.navbar}>Navigation</nav>;
+  return (
+    <nav className={classes.navbar}>
+      <NavLink to="home">Home</NavLink>
+      <NavLink to="notes">Notes</NavLink>
+      <NavLink to="todos">Todos</NavLink>
+      <NavLink to="account">Account</NavLink>
+      <NavLink to="auth">Auth</NavLink>
+    </nav>
+  );
 };
 
 export default Navbar;
