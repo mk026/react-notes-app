d9c450e9f0ab757da126e323990549ab74fda311
Mikhail <mk0602604@gmail.com>
2022-04-30 17:17:22 +0300

    Add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
-  return <div>React Notes App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
