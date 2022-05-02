9e012cfe6e7bf8975d192ebf4d290cfe82d629b9
Mikhail <mk0602604@gmail.com>
2022-05-02 17:01:37 +0300

    Create auth slice

---
+++ src/store/reducers/authReducer.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface AuthState {
+  isAuth: boolean;
+}
+
+const initialState: AuthState = {
+  isAuth: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login(state) {
+      state.isAuth = true;
+    },
+    logout(state) {
+      state.isAuth = false;
+    },
+  },
+});
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./reducers/authReducer";
 
-const rootReducer = () => {};
+const rootReducer = combineReducers({
+  auth: authSlice.reducer,
+});
 
 export const setupStore = () => configureStore({ reducer: rootReducer });
