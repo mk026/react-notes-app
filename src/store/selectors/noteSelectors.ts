c9ec0b8eb6ce4c3181df79fbb401717d97dc279c
Mikhail <mk0602604@gmail.com>
2022-05-03 19:19:39 +0300

    Create state selectors

---
+++ src/store/selectors/noteSelectors.ts
@@ -0,0 +1,3 @@
+import { RootState } from "..";
+
+export const getNoteState = (state: RootState) => state.note;
---
+++ src/store/selectors/todoSelectors.ts
@@ -0,0 +1,3 @@
+import { RootState } from "..";
+
+export const getTodoState = (state: RootState) => state.todo;
