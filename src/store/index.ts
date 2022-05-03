2e4d1d9150353ad4f3aee4db4a824e0ced7c2e30
Mikhail <mk0602604@gmail.com>
2022-05-03 18:45:41 +0300

    Create custom redux hooks

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,3 +11,7 @@ const rootReducer = combineReducers({
 });
 
 export const setupStore = () => configureStore({ reducer: rootReducer });
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
