f0e378fbd1e83bdd1f9460c120793b76fd8513ee
Mikhail <mk0602604@gmail.com>
2022-05-06 23:04:48 +0300

    Create another axios instance for auth requests

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,15 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import AuthService from "../services/AuthService";
 
 export const API_URL = "http://localhost:8080/api";
 
 export const api = axios.create({ baseURL: API_URL });
+
+export const authApi = axios.create({ baseURL: API_URL });
+
+const authInterceptor = (config: AxiosRequestConfig) => {
+  config.headers!.Authorization = `Bearer ${AuthService.getStoredToken()}`;
+  return config;
+};
+
+authApi.interceptors.request.use(authInterceptor);
