dc1fce9b98189733bfeae707cfc618d47f501bb6
Mikhail <mk0602604@gmail.com>
2022-05-06 13:48:02 +0300

    Add logic to signup form component

--- src/components/forms/signup-form/SignupForm.tsx
+++ src/components/forms/signup-form/SignupForm.tsx
@@ -1,16 +1,52 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
+import { useAppDispatch } from "../../../hooks/redux";
+import { signup } from "../../../store/action-creators/authActionCreators";
 
 const SignupForm: FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const dispatch = useAppDispatch();
+
+  const signupHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password === passwordConfirm) {
+      dispatch(signup(name, email, password));
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={signupHandler}>
       <label htmlFor="signup-name">Enter your name</label>
-      <input id="signup-name" type="text" />
+      <input
+        id="signup-name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label htmlFor="signup-email">Enter your email</label>
-      <input id="signup-email" type="text" />
+      <input
+        id="signup-email"
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <label htmlFor="signup-password">Enter your password</label>
-      <input id="signup-password" type="text" />
+      <input
+        id="signup-password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <label htmlFor="signup-password-confirm">Confirm your password</label>
-      <input id="signup-password-confirm" type="text" />
+      <input
+        id="signup-password-confirm"
+        type="password"
+        value={passwordConfirm}
+        onChange={(e) => setPasswordConfirm(e.target.value)}
+      />
+      <button type="submit">Signup</button>
     </form>
   );
 };
