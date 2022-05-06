fdd4787439568a343b862356e1256dd8179ea446
Mikhail <mk0602604@gmail.com>
2022-05-06 16:57:46 +0300

    Check auth token on app start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import Navbar from "./components/navbar/Navbar";
 import AppRouter from "./components/app-router/AppRouter";
+import { useEffect } from "react";
+import AuthService from "./services/AuthService";
+import { useAppDispatch } from "./hooks/redux";
+import { authSlice } from "./store/reducers/authReducer";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (AuthService.getStoredToken()) {
+      dispatch(authSlice.actions.signinSuccess());
+    }
+  }, [dispatch]);
+
   return (
     <>
       <Navbar />
