7af83ed58744a2a7d80a6b893056fd26de1bb3d3
Mikhail <mk0602604@gmail.com>
2022-05-06 13:13:05 +0300

    Add logic to signin form component

--- src/components/forms/signin-form/SigninForm.tsx
+++ src/components/forms/signin-form/SigninForm.tsx
@@ -1,12 +1,34 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
+import { useAppDispatch } from "../../../hooks/redux";
+import { signin } from "../../../store/action-creators/authActionCreators";
 
 const SigninForm: FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useAppDispatch();
+
+  const signinHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(signin(email, password));
+  };
+
   return (
-    <form>
-      <label htmlFor="signin-name">Enter your name</label>
-      <input id="signin-name" type="text" />
+    <form onSubmit={signinHandler}>
+      <label htmlFor="signin-email">Enter your email</label>
+      <input
+        id="signin-email"
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <label htmlFor="signin-password">Enter your password</label>
-      <input id="signin-password" type="text" />
+      <input
+        id="signin-password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Signin</button>
     </form>
   );
 };
