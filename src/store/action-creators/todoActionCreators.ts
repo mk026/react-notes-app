75b0be9c6284f9ec03a0693f3c5a181af6773a9b
Mikhail <mk0602604@gmail.com>
2022-05-07 15:01:08 +0300

    Add fetchTodos reducers and action creator

---
+++ src/store/action-creators/todoActionCreators.ts
@@ -0,0 +1,13 @@
+import { AppDispatch } from "..";
+import TodoService from "../../services/TodoService";
+import { todoSlice } from "../reducers/todoReducer";
+
+export const fetchTodos = () => async (dispatch: AppDispatch) => {
+  try {
+    dispatch(todoSlice.actions.fetchTodos());
+    const response = await TodoService.fetchTodos();
+    dispatch(todoSlice.actions.fetchTodosSuccess(response.data.notes));
+  } catch (e) {
+    dispatch(todoSlice.actions.fetchTodosError((e as Error).message));
+  }
+};
--- src/store/reducers/todoReducer.ts
+++ src/store/reducers/todoReducer.ts
@@ -1,17 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { ITodo } from "../../models/ITodo";
 
 interface TodosState {
   todos: ITodo[];
+  isLoading: boolean;
+  error: string | null;
 }
 
 const initialState: TodosState = {
   todos: [],
+  isLoading: false,
+  error: null,
 };
 
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
-  reducers: {},
+  reducers: {
+    fetchTodos(state) {
+      state.isLoading = true;
+    },
+    fetchTodosSuccess(state, action: PayloadAction<ITodo[]>) {
+      state.isLoading = false;
+      state.todos = action.payload;
+    },
+    fetchTodosError(state, action: PayloadAction<string>) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
 });
