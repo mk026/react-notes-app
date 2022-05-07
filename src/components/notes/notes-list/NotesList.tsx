009f1bd49bbf859ebfbcca332defb646662fea73
Mikhail <mk0602604@gmail.com>
2022-05-07 16:14:50 +0300

    Fetch and render notes in notes list component

--- src/components/notes/notes-list/NotesList.tsx
+++ src/components/notes/notes-list/NotesList.tsx
@@ -1,12 +1,36 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
+import { fetchNotes } from "../../../store/action-creators/noteActionCreators";
+import { getNoteState } from "../../../store/selectors/noteSelectors";
+import NoteItem from "../note-item/NoteItem";
 
 const NotesList: FC = () => {
+  const { notes, isLoading, error } = useAppSelector(getNoteState);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchNotes());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!notes.length) {
+    return <div>No notes found</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
-    <ul>
-      <li>Note 1</li>
-      <li>Note 2</li>
-      <li>Note 3</li>
-    </ul>
+    <div>
+      {notes.map((note) => (
+        <NoteItem key={note.id} note={note} />
+      ))}
+    </div>
   );
 };
 
