e31fb720c194a70563933ebef1ff5a4e5f4790b9
Mikhail <mk0602604@gmail.com>
2022-05-07 14:52:35 +0300

    Add fetchNotes reducers and action creator

---
+++ src/store/action-creators/noteActionCreators.ts
@@ -0,0 +1,13 @@
+import { AppDispatch } from "..";
+import NoteService from "../../services/NoteService";
+import { noteSlice } from "../reducers/noteReducer";
+
+export const fetchNotes = () => async (dispatch: AppDispatch) => {
+  try {
+    dispatch(noteSlice.actions.fetchNotes());
+    const response = await NoteService.fetchNotes();
+    dispatch(noteSlice.actions.fetchNotesSuccess(response.data.notes));
+  } catch (e) {
+    dispatch(noteSlice.actions.fetchNotesError((e as Error).message));
+  }
+};
--- src/store/reducers/noteReducer.ts
+++ src/store/reducers/noteReducer.ts
@@ -1,17 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { INote } from "../../models/INote";
 
 interface NotesState {
   notes: INote[];
+  isLoading: boolean;
+  error: string | null;
 }
 
 const initialState: NotesState = {
   notes: [],
+  isLoading: false,
+  error: null,
 };
 
 export const noteSlice = createSlice({
   name: "note",
   initialState,
-  reducers: {},
+  reducers: {
+    fetchNotes(state) {
+      state.isLoading = true;
+    },
+    fetchNotesSuccess(state, action: PayloadAction<INote[]>) {
+      state.isLoading = false;
+      state.notes = action.payload;
+    },
+    fetchNotesError(state, action: PayloadAction<string>) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
 });
