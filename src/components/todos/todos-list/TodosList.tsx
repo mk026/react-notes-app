9016b43a30f31f141faeb057db752d69f9af6ce2
Mikhail <mk0602604@gmail.com>
2022-05-07 17:06:34 +0300

    Fetch and render todos in todos list component

--- src/components/todos/todos-list/TodosList.tsx
+++ src/components/todos/todos-list/TodosList.tsx
@@ -1,12 +1,36 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
+import { fetchTodos } from "../../../store/action-creators/todoActionCreators";
+import { getTodoState } from "../../../store/selectors/todoSelectors";
+import TodoItem from "../todo-item/TodoItem";
 
 const TodosList: FC = () => {
+  const { todos, isLoading, error } = useAppSelector(getTodoState);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!todos.length) {
+    return <div>No todos found</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
-    <ul>
-      <li>Todo 1</li>
-      <li>Todo 2</li>
-      <li>Todo 3</li>
-    </ul>
+    <div>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </div>
   );
 };
 
