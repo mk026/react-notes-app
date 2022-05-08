1019192d2e290a135ac102a77a01e624d1e7edfd
Mikhail <mk0602604@gmail.com>
2022-05-08 23:26:11 +0300

    Add logic to addTodo form component

--- src/components/forms/add-todo-form/AddTodoForm.tsx
+++ src/components/forms/add-todo-form/AddTodoForm.tsx
@@ -1,10 +1,27 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
+
+import { useAppDispatch } from "../../../hooks/redux";
+import { addTodo } from "../../../store/action-creators/todoActionCreators";
 
 const AddTodoForm: FC = () => {
+  const [title, setTitle] = useState("");
+  const dispatch = useAppDispatch();
+
+  const addTodoHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(addTodo(title));
+  };
+
   return (
-    <form>
+    <form onSubmit={addTodoHandler}>
       <label htmlFor="todo-title">Todo title</label>
-      <input id="todo-title" type="text" />
+      <input
+        id="todo-title"
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button type="submit">Add todo</button>
     </form>
   );
 };
