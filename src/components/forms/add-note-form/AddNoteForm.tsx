8fa4edb182046eec8736218adbf26d5a54537a52
Mikhail <mk0602604@gmail.com>
2022-05-08 23:20:47 +0300

    Add logic to addNote form component

--- src/components/forms/add-note-form/AddNoteForm.tsx
+++ src/components/forms/add-note-form/AddNoteForm.tsx
@@ -1,12 +1,35 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
+
+import { useAppDispatch } from "../../../hooks/redux";
+import { addNote } from "../../../store/action-creators/noteActionCreators";
 
 const AddNoteForm: FC = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const dispatch = useAppDispatch();
+
+  const addNoteHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(addNote(title, content));
+  };
+
   return (
-    <form>
+    <form onSubmit={addNoteHandler}>
       <label htmlFor="note-title">Note title</label>
-      <input id="note-title" type="text" />
+      <input
+        id="note-title"
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <label htmlFor="note-content">Note content</label>
-      <input id="note-content" type="text" />
+      <input
+        id="note-content"
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button type="submit">Add todo</button>
     </form>
   );
 };
