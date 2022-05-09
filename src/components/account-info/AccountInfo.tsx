af2a6d8a87638c9e8ace51f362048a8ec0e685e3
Mikhail <mk0602604@gmail.com>
2022-05-09 16:05:09 +0300

    Add logic to account info component
    Display user info and add change name/email handlers

--- src/components/account-info/AccountInfo.tsx
+++ src/components/account-info/AccountInfo.tsx
@@ -1,23 +1,63 @@
-import { FC, MouseEvent } from "react";
+import { FC, FormEvent, MouseEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { useAppDispatch } from "../../hooks/redux";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { Paths } from "../../routes/types";
-import { authSlice } from "../../store/reducers/authReducer";
+import { signout } from "../../store/action-creators/authActionCreators";
+import {
+  updateEmail,
+  updateName,
+} from "../../store/action-creators/userActionCreators";
+import { getUserState } from "../../store/selectors/userSelectors";
 
 const AccountInfo: FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const { user } = useAppSelector(getUserState);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   const signoutHandler = (e: MouseEvent<HTMLButtonElement>) => {
-    dispatch(authSlice.actions.signout());
+    dispatch(signout());
     navigate(Paths.HOME_PATH);
+  };
+
+  const changeNameHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(updateName(name));
+  };
+
+  const changeEmailHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(updateEmail(email));
   };
 
   return (
     <div>
-      <p>User Name</p>
-      <p>User Email</p>
+      <div>
+        <p>{user?.name}</p>
+        <form onSubmit={changeNameHandler}>
+          <input
+            type="text"
+            placeholder="Change name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit">Save</button>
+        </form>
+      </div>
+      <div>
+        <p>{user?.email}</p>
+        <form onSubmit={changeEmailHandler}>
+          <input
+            type="text"
+            placeholder="Change email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button type="submit">Save</button>
+        </form>
+      </div>
       <button onClick={signoutHandler}>Signout</button>
     </div>
   );
