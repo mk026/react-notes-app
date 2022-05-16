596782f82d2c87fd117fcc665a4f83c3c7a29acf
Mikhail <mk0602604@gmail.com>
2022-05-16 22:22:14 +0300

    Create card component

---
+++ src/components/ui/card/Card.tsx
@@ -0,0 +1,13 @@
+import { FC, ReactNode } from "react";
+
+import classes from "./Card.module.css";
+
+interface CardProps {
+  children?: ReactNode;
+}
+
+const Card: FC<CardProps> = ({ children }) => {
+  return <div className={classes.card}>{children}</div>;
+};
+
+export default Card;
