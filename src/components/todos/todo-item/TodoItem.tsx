a22100b69c20b06f316af40e9dc6e02c8fbca1ca
Mikhail <mk0602604@gmail.com>
2022-05-23 20:06:55 +0300

    Add button to show edit todo form

--- src/components/todos/todo-item/TodoItem.tsx
+++ src/components/todos/todo-item/TodoItem.tsx
@@ -1,16 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+
 import { ITodo } from "../../../models/ITodo";
+import EditTodoForm from "../../forms/edit-todo-form/EditTodoForm";
 
 interface TodoItemProps {
   todo: ITodo;
 }
 
 const TodoItem: FC<TodoItemProps> = ({ todo }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEditForm = () => setIsEditing((prev) => !prev);
+
   return (
-    <div>
-      <input type="checkbox" checked={todo.completed} />
-      <p>{todo.title}</p>
-    </div>
+    <>
+      {isEditing && <EditTodoForm todo={todo} />}
+      <div>
+        <input type="checkbox" checked={todo.completed} />
+        <p>{todo.title}</p>
+        <button onClick={toggleEditForm}>Edit</button>
+      </div>
+    </>
   );
 };
 
