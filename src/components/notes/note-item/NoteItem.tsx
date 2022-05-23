fcba006cbda4d2cffb65e4adfd318023b6723aa8
Mikhail <mk0602604@gmail.com>
2022-05-23 20:03:03 +0300

    Add button to show edit note form

--- src/components/notes/note-item/NoteItem.tsx
+++ src/components/notes/note-item/NoteItem.tsx
@@ -1,16 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+
 import { INote } from "../../../models/INote";
+import EditNoteForm from "../../forms/edit-note-form/EditNoteForm";
 
 interface NoteItemProps {
   note: INote;
 }
 
 const NoteItem: FC<NoteItemProps> = ({ note }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEditForm = () => setIsEditing((prev) => !prev);
+
   return (
-    <div>
-      <p>{note.title}</p>
-      <p>{note.content}</p>
-    </div>
+    <>
+      {isEditing && <EditNoteForm note={note} />}
+      <div>
+        <p>{note.title}</p>
+        <p>{note.content}</p>
+        <button onClick={toggleEditForm}>Edit</button>
+      </div>
+    </>
   );
 };
 
