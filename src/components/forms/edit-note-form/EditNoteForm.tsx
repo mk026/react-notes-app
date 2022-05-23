c98656145647cb45f3fc1be735ca487651632aa5
Mikhail <mk0602604@gmail.com>
2022-05-23 19:46:48 +0300

    Add logic to edit note form component

--- src/components/forms/edit-note-form/EditNoteForm.tsx
+++ src/components/forms/edit-note-form/EditNoteForm.tsx
@@ -1,12 +1,38 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
 
-const EditNoteForm: FC = () => {
+import { useActions } from "../../../hooks/useActions";
+import { INote } from "../../../models/INote";
+
+interface EditNoteFormProps {
+  note: INote;
+}
+
+const EditNoteForm: FC<EditNoteFormProps> = ({ note }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const { updateNote } = useActions();
+
+  const editNoteHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    updateNote({ _id: note._id, title, content });
+  };
+
   return (
-    <form>
+    <form onSubmit={editNoteHandler}>
       <label htmlFor="edit-note-title">New note title</label>
-      <input id="edit-note-title" type="text" />
+      <input
+        id="edit-note-title"
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <label htmlFor="edit-note-content">New note content</label>
-      <input id="edit-note-content" type="text" />
+      <input
+        id="edit-note-content"
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
       <button type="submit">Save</button>
     </form>
   );
