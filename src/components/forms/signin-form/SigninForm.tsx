37d174d9078e7f85dada5d07ba4a375eb2bd6a64
Mikhail <mk0602604@gmail.com>
2022-05-25 21:39:30 +0300

    Use formik and yup for signin form validation

--- src/components/forms/signin-form/SigninForm.tsx
+++ src/components/forms/signin-form/SigninForm.tsx
@@ -1,4 +1,6 @@
-import { FC, FormEvent, useState } from "react";
+import { FC } from "react";
+import { Formik } from "formik";
+import * as yup from "yup";
 
 import { useActions } from "../../../hooks/useActions";
 
@@ -6,37 +8,68 @@ interface SigninFormProps {
   switchToSignup: () => void;
 }
 
+interface SigninFormValues {
+  email: string;
+  password: string;
+}
+
 const SigninForm: FC<SigninFormProps> = ({ switchToSignup }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const { signin } = useActions();
 
-  const signinHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const initialValues: SigninFormValues = { email: "", password: "" };
+  const validationSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  });
+
+  const signinHandler = ({ email, password }: SigninFormValues) => {
     signin(email, password);
   };
 
   return (
-    <form onSubmit={signinHandler}>
-      <label htmlFor="signin-email">Enter your email</label>
-      <input
-        id="signin-email"
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label htmlFor="signin-password">Enter your password</label>
-      <input
-        id="signin-password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Signin</button>
-      <p>
-        Don't have an account? <button onClick={switchToSignup}>Signup</button>
-      </p>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={signinHandler}
+      validateOnBlur
+    >
+      {({
+        values,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        errors,
+        touched,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          {touched.email && errors.email && <div>{errors.email}</div>}
+          <label htmlFor="signin-email">Enter your email</label>
+          <input
+            id="signin-email"
+            name="email"
+            type="text"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {touched.password && errors.password && <div>{errors.password}</div>}
+          <label htmlFor="signin-password">Enter your password</label>
+          <input
+            id="signin-password"
+            name="password"
+            type="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <button type="submit">Signin</button>
+          <p>
+            Don't have an account?{" "}
+            <button onClick={switchToSignup}>Signup</button>
+          </p>
+        </form>
+      )}
+    </Formik>
   );
 };
 
