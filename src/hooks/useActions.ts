589c77fbd8b26df23ace206979ca7498e95139aa
Mikhail <mk0602604@gmail.com>
2022-06-08 21:45:16 +0300

    Refactor useActions hook
    Use all actions from slices, not just custom action-creators

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,11 +1,25 @@
+import { useMemo } from "react";
 import { bindActionCreators } from "@reduxjs/toolkit";
 
 import { useAppDispatch } from "./redux";
-import * as actions from "../store/action-creators";
-import { useMemo } from "react";
+import * as actionCreators from "../store/action-creators";
+import { authSlice, noteSlice, todoSlice, userSlice } from "../store/reducers";
 
 export const useActions = () => {
   const dispatch = useAppDispatch();
 
-  return useMemo(() => bindActionCreators(actions, dispatch), [dispatch]);
+  return useMemo(
+    () =>
+      bindActionCreators(
+        {
+          ...actionCreators,
+          ...authSlice.actions,
+          ...userSlice.actions,
+          ...noteSlice.actions,
+          ...todoSlice.actions,
+        },
+        dispatch
+      ),
+    [dispatch]
+  );
 };
