d40d63eb01353c1e85c91b35fa3d2b02916fa5c6
Mikhail <mk0602604@gmail.com>
2022-06-16 01:56:08 +0300

    Make notes expandable

--- src/components/notes/note-item/NoteItem.tsx
+++ src/components/notes/note-item/NoteItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import Card from "../../ui/card/Card";
 import Button from "../../ui/button/Button";
@@ -12,20 +12,35 @@ interface NoteItemProps {
 }
 
 const NoteItem: FC<NoteItemProps> = ({ note }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
   const { showEditNoteForm, deleteNote } = useActions();
+
+  const isExpandable = note.content.length > 100;
+
+  const contentStyles = [classes["note__content"]];
+
+  if (isExpandable && !isExpanded) {
+    contentStyles.push(classes["note__content--preview"]);
+  }
 
   const editNoteHandler = () => showEditNoteForm(note);
   const deleteNoteHandler = () => deleteNote(note._id);
+  const toggleFullNoteHandler = () => setIsExpanded((prev) => !prev);
 
   return (
     <>
-      <Card className={classes.note}>
+      <Card className={classes["note"]}>
         <p className={classes["note__title"]}>{note.title}</p>
         <hr className={classes["note__hr"]} />
-        <p>{note.content}</p>
+        <p className={contentStyles.join(" ")}>{note.content}</p>
         <div className={classes["note__controls"]}>
           <Button onClick={editNoteHandler}>Edit</Button>
           <Button onClick={deleteNoteHandler}>Delete</Button>
+          {isExpandable && (
+            <Button onClick={toggleFullNoteHandler}>
+              {isExpanded ? "Hide" : "Expand"}
+            </Button>
+          )}
         </div>
       </Card>
     </>
