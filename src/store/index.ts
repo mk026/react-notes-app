d7fa08495931790b9d16eaa012c6da9331050b43
Mikhail <mk0602604@gmail.com>
2022-07-12 15:02:09 +0300

    Register image slice and add image selector

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,19 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
-import { authSlice, userSlice, noteSlice, todoSlice } from "./reducers";
+import {
+  authSlice,
+  userSlice,
+  noteSlice,
+  todoSlice,
+  imageSlice,
+} from "./reducers";
 
 const rootReducer = combineReducers({
   auth: authSlice.reducer,
   user: userSlice.reducer,
   note: noteSlice.reducer,
   todo: todoSlice.reducer,
+  image: imageSlice.reducer,
 });
 
 export const setupStore = () => configureStore({ reducer: rootReducer });
---
+++ src/store/selectors/imageSelectors.ts
@@ -0,0 +1,3 @@
+import { RootState } from "..";
+
+export const getImageState = (state: RootState) => state.image;
