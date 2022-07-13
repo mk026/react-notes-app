f0ba42b79624972fd631e91c95fc7cf58b1dfd5a
Mikhail <mk0602604@gmail.com>
2022-07-13 23:30:52 +0300

    Add update and delete image service methods

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -7,4 +7,12 @@ export default class ImageService {
   static async fetchImages(): Promise<AxiosResponse<IImage[]>> {
     return authApi.get<IImage[]>(ApiEndpoints.IMAGES);
   }
+
+  static async updateImage(image: IImage): Promise<AxiosResponse<IImage>> {
+    return authApi.get<IImage>(ApiEndpoints.IMAGES, image);
+  }
+
+  static async deleteImage(id: string): Promise<AxiosResponse<IImage>> {
+    return authApi.delete<IImage>(`${ApiEndpoints.IMAGES}/${id}`);
+  }
 }
