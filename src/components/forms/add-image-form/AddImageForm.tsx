3995a9318d9aceb0b9debf676c26187924af727e
Mikhail <mk0602604@gmail.com>
2022-07-15 21:35:40 +0300

    Use formik in add-image-form component

--- src/components/forms/add-image-form/AddImageForm.tsx
+++ src/components/forms/add-image-form/AddImageForm.tsx
@@ -1,22 +1,67 @@
 import { FC } from "react";
+import { useFormik } from "formik";
 
 import Form from "../../ui/form/Form";
 import Button from "../../ui/button/Button";
 import Input from "../../ui/input/Input";
 import Textarea from "../../ui/textarea/Textarea";
+import {
+  imageFormInitialValues,
+  ImageFormValues,
+  imageValidationSchema,
+} from "../../../validation/imageValidation";
 
 interface AddImageFormProps {
   onClose: () => void;
 }
 
 const AddImageForm: FC<AddImageFormProps> = ({ onClose }) => {
+  const {
+    values,
+    touched,
+    errors,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    isValid,
+  } = useFormik<ImageFormValues>({
+    initialValues: imageFormInitialValues,
+    validationSchema: imageValidationSchema,
+    onSubmit: (values) => addImageHandler(values),
+  });
+
+  const addImageHandler = ({ title, description }: ImageFormValues) => {
+    onClose();
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <label htmlFor="title">Image title</label>
-      <Input type="text" />
+      <Input
+        id="title"
+        name="title"
+        type="text"
+        value={values.title}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        touched={touched.title}
+        error={errors.title}
+      />
       <label htmlFor="description">Image description</label>
-      <Textarea id="description" cols={40} rows={6} />
-      <Button type="submit">Add image</Button>
+      <Textarea
+        id="description"
+        name="description"
+        value={values.description}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        touched={touched.description}
+        error={errors.description}
+        cols={40}
+        rows={6}
+      />
+      <Button type="submit" disabled={!isValid}>
+        Add image
+      </Button>
       <Button type="button" onClick={onClose}>
         Cancel
       </Button>
