7bda621fddd64ffef6062104901feefc2da76571
Mikhail <mk0602604@gmail.com>
2022-07-15 20:47:51 +0300

    Render add-image-form on gallery page

--- src/pages/gallery-page/GalleryPage.tsx
+++ src/pages/gallery-page/GalleryPage.tsx
@@ -1,16 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import AddImageForm from "../../components/forms/add-image-form/AddImageForm";
 
 import Button from "../../components/ui/button/Button";
+import Drawer from "../../components/ui/drawer/Drawer";
 import PageTop from "../../components/ui/page-top/PageTop";
 import Page from "../../components/ui/page/Page";
 
 const GalleryPage: FC = () => {
+  const [isAddingImage, setIsAddingImage] = useState(false);
+
+  const toggleAddImageForm = () => setIsAddingImage((prev) => !prev);
+
   return (
-    <Page>
-      <PageTop title="My Gallery">
-        <Button>Add new image</Button>
-      </PageTop>
-    </Page>
+    <>
+      <Drawer
+        isActive={isAddingImage}
+        onClose={toggleAddImageForm}
+        position="right"
+      >
+        <AddImageForm onClose={toggleAddImageForm} />
+      </Drawer>
+      <Page>
+        <PageTop title="My Gallery">
+          <Button onClick={toggleAddImageForm}>Add new image</Button>
+        </PageTop>
+      </Page>
+    </>
   );
 };
 
