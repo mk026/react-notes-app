7f3a8c8b8c7756a7fdc30fa1dbb5fec5869c33b4
Mikhail <mk0602604@gmail.com>
2022-07-23 21:24:40 +0300

    Add handlers for edit-image-form

--- src/components/forms/edit-image-form/EditImageForm.tsx
+++ src/components/forms/edit-image-form/EditImageForm.tsx
@@ -4,16 +4,77 @@ import Form from "../../ui/form/Form";
 import Input from "../../ui/input/Input";
 import Textarea from "../../ui/textarea/Textarea";
 import Button from "../../ui/button/Button";
+import IImage from "../../../models/IImage";
+import { useActions } from "../../../hooks/useActions";
+import { imageActions } from "../../../store/action-creators";
+import { useFormik } from "formik";
+import {
+  ImageFormValues,
+  imageValidationSchema,
+} from "../../../validation/imageValidation";
 
-const EditImageForm: FC = () => {
+interface EditImageFormProps {
+  image: IImage;
+  onClose: () => void;
+}
+
+const EditImageForm: FC<EditImageFormProps> = ({ image, onClose }) => {
+  const { updateImage } = useActions(imageActions);
+
+  const {
+    values,
+    touched,
+    errors,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    isValid,
+  } = useFormik<ImageFormValues>({
+    initialValues: {
+      title: image.title,
+      description: image.description,
+    },
+    validationSchema: imageValidationSchema,
+    onSubmit: (values) => editImageHandler(values),
+    enableReinitialize: true,
+  });
+
+  const editImageHandler = ({ title, description }: ImageFormValues) => {
+    updateImage({ ...image, title, description });
+    onClose();
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <label htmlFor="title">New image title</label>
-      <Input id="title" name="title" type="text" />
-      <label htmlFor="content">New image description</label>
-      <Textarea id="description" name="description" cols={40} rows={6} />
-      <Button type="submit">Save</Button>
-      <Button type="button">Cancel</Button>
+      <Input
+        id="title"
+        name="title"
+        type="text"
+        value={values.title}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        touched={touched.title}
+        error={errors.title}
+      />
+      <label htmlFor="description">New image description</label>
+      <Textarea
+        id="description"
+        name="description"
+        value={values.description}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        touched={touched.description}
+        error={errors.description}
+        cols={40}
+        rows={6}
+      />
+      <Button type="submit" disabled={!isValid}>
+        Save
+      </Button>
+      <Button type="button" onClick={onClose}>
+        Cancel
+      </Button>
     </Form>
   );
 };
